c9f7294ea2fa0f94638c7dd3bb20ee6df31b0532
Vardnan <46565659+vardnan@users.noreply.github.com>
2020-09-24 18:11:26 +0100

    increase responsiveness, and improve ux of accordion

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -8,6 +8,7 @@ import { motion } from "framer-motion";
 
 const transition = { duration: 0.8, ease: [0.6, 0.01, -0.05, 0.9] };
 
+
 function createCard(image) {
   return (
     <Link to={image.link}>
