094dbeec4b83d84de1c1187dc6a077709674aec7
Vardnan <46565659+vardnan@users.noreply.github.com>
2020-09-26 22:32:46 +0100

    update mobileweb section

--- src/Main.jsx
+++ src/Main.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 
-import Home from "../pages/Home";
-import UoM from "../pages/UoM";
-import Antler from "../pages/Antler";
-import ProductDesign from "../pages/ProductDesign";
-import NordicImpact from "../pages/NordicImpact";
-import MobileWebDevelopment from "../pages/MobileWebDevelopment";
-import Jor from "../pages/Jor";
-import About from "../pages/About";
+import Home from "./pages/Home";
+import UoM from "./pages/UoM";
+import Antler from "./pages/Antler";
+import ProductDesign from "./pages/ProductDesign";
+import NordicImpact from "./pages/NordicImpact";
+import MobileWebDevelopment from "./pages/MobileWebDevelopment";
+import Jor from "./pages/Jor";
+import About from "./pages/About";
 
 const Main = () => {
   return (
