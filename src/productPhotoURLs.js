56d4166350306fa660cff02366775927ea91d464
Vardnan <46565659+vardnan@users.noreply.github.com>
2020-10-11 10:15:07 +0100

    update product page

--- src/productPhotoURLs.js
+++ src/productPhotoURLs.js
@@ -1,34 +1,41 @@
 const product_photos = [
   {
     id: 1,
+    title: "Streamline | Meeting Management Tool",
+    gif: "gif",
+    url:
+      "https://uploads.codesandbox.io/uploads/user/fd760ebc-a259-4eec-b792-8e672dacc7d4/wtvs-Streamline-min.jpg"
+  },
+  {
+    id: 2,
     title: "COVID-19 | World Health Organisation | Exploration",
     gif: "gif",
     url:
       "https://uploads.codesandbox.io/uploads/user/fd760ebc-a259-4eec-b792-8e672dacc7d4/NAgH-covid19_productPhoto-min.jpg"
   },
   {
-    id: 2,
+    id: 3,
     title: "Music Player | Neumorphism",
     gif: "gif",
     url:
       "https://uploads.codesandbox.io/uploads/user/fd760ebc-a259-4eec-b792-8e672dacc7d4/HGZA-music_productPhoto-min.jpg"
   },
   {
-    id: 3,
+    id: 4,
     title: "Youtube | Redesigned",
     gif: "gif",
     url:
       "https://uploads.codesandbox.io/uploads/user/fd760ebc-a259-4eec-b792-8e672dacc7d4/cIHa-youtube_productPhoto-min.jpg"
   },
   {
-    id: 4,
+    id: 5,
     title: "Credit Suisse Wealth Management App | Light Mode",
     gif: "gif",
     url:
       "https://uploads.codesandbox.io/uploads/user/fd760ebc-a259-4eec-b792-8e672dacc7d4/PDZY-credit_productPhoto-min.jpg"
   },
   {
-    id: 5,
+    id: 6,
     title: "Spotify-ish | Exploration",
     gif: "gif",
     url:
