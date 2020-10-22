0096a6f615141ca252190bf0377b34f0e076b213
Vardnan <46565659+vardnan@users.noreply.github.com>
2020-10-22 21:43:38 +0100

    add ame podcast cover

--- src/productPhotoURLs.js
+++ src/productPhotoURLs.js
@@ -40,6 +40,13 @@ const product_photos = [
     gif: "gif",
     url:
       "https://uploads.codesandbox.io/uploads/user/fd760ebc-a259-4eec-b792-8e672dacc7d4/DWff-Rectangle+6-min.jpg"
+  },
+  {
+    id: 7,
+    title: "Startup Podcast Cover | AccelerateME",
+    gif: "gif",
+    url:
+      "https://uploads.codesandbox.io/uploads/user/fd760ebc-a259-4eec-b792-8e672dacc7d4/MW0O-iPhone+XS+-+Silver-min.jpg"
   }
 ];
 export default product_photos;
